afe1f934ac9e1c02e86b2bf45ad61c7d9f96c58a
Emmanuel Montoya <emmanuel.montoyae@gmail.com>
2026-07-21 12:55:10 -0600

    chore: remove obsolete .data from FontAwesomeIcons in settings page

--- lib/ui/settings_and_privacy_page.dart
+++ lib/ui/settings_and_privacy_page.dart
@@ -73,7 +73,7 @@ class SettingsPrivacyPage extends StatelessWidget {
                               mainAxisSize: MainAxisSize.min,
                               children: [
                                 IconButton(
-                                  icon: Icon(FontAwesomeIcons.envelope.data,),
+                                  icon: Icon(FontAwesomeIcons.envelope,),
                                   iconSize: 40,
                                   tooltip: SettingTranslationConstants.gmail.tr,
                                   onPressed: () {
@@ -114,7 +114,7 @@ class SettingsPrivacyPage extends StatelessWidget {
                               mainAxisSize: MainAxisSize.min,
                               children: [
                                 IconButton(
-                                  icon: Icon(FontAwesomeIcons.instagram.data,),
+                                  icon: Icon(FontAwesomeIcons.instagram,),
                                   iconSize: 40,
                                   tooltip: AppTranslationConstants.instagram.tr,
                                   onPressed: () {
@@ -177,7 +177,7 @@ class SettingsPrivacyPage extends StatelessWidget {
                                 mainAxisSize: MainAxisSize.min,
                                 children: [
                                   IconButton(
-                                    icon: Icon(FontAwesomeIcons.whatsapp.data,),
+                                    icon: Icon(FontAwesomeIcons.whatsapp,),
                                     iconSize: 40,
                                     tooltip: SettingTranslationConstants.whatsCommunity.tr,
                                     onPressed: () {
@@ -197,7 +197,7 @@ class SettingsPrivacyPage extends StatelessWidget {
                                 mainAxisSize: MainAxisSize.min,
                                 children: [
                                   IconButton(
-                                    icon: Icon(FontAwesomeIcons.whatsapp.data,),
+                                    icon: Icon(FontAwesomeIcons.whatsapp,),
                                     iconSize: 40,
                                     tooltip: SettingTranslationConstants.whatsCommunity.tr,
                                     onPressed: () {
